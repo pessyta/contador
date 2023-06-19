69173ae15f3cda35e920dce7cd7e13e0873d7522
pessyta <jessym_@hotmail.com>
2023-06-19 11:50:16 -0600

    cambios script pa que jale

--- src/temporizador.js
+++ src/temporizador.js
@@ -95,3 +95,30 @@ console.log(tiempo);
 
 
 */
+
+let timer = 0;
+let intervalId;
+
+function startTimer() {
+  clearInterval(intervalId);
+  let inputTime = 0; 
+  inputTime = parseInt(document.getElementById('inputTime').value);
+  if (isNaN(inputTime) || inputTime <= 0) {
+      alert('Ingrese tiempo en segundos.');
+      return;
+  }
+
+  timer = inputTime;
+  document.getElementById('timer').innerHTML = timer;
+  intervalId = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+  timer--;
+  document.getElementById('timer').innerHTML = timer;
+  if (timer === 0) {
+      document.getElementById('timer').innerHTML = timer;      
+      clearInterval(intervalId);
+      alert('¡SE ACABO!');
+  }
+}
